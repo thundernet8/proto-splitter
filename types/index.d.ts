66f4af587b2aceacb3396e579b1e4f33df81b93a
thundernet8 <wuxueqian2010@hotmail.com>
2020-08-31 12:00:21 +0800

    fix: npm config

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,5 @@
 import * as Protobuf from 'protobufjs';
+import { Options } from '@grpc/proto-loader';
 interface MessageDefinition<T = Protobuf.Message> {
     outName?: string;
     fullName: string;
@@ -16,11 +17,16 @@ export interface ProtoRoute {
     enums: MessageDefinition<Protobuf.Enum>[];
     errors: string[];
 }
-export interface GetProtoRoutesOptions {
+export interface GetProtoRoutesOptions extends Options {
     /**
      * 是否格式化proto文件，默认否则proto文件会被压缩
      */
     format?: boolean;
 }
+/**
+ * 从单个proto文件中解析出以单个RPC方法为粒度的proto文件列表
+ * @param filePath proto文件路径
+ * @param options proto文件解析以及输出选项，主要包括includeDirs和format选项
+ */
 export declare function getProtoRoutes(filePath: string, options?: GetProtoRoutesOptions): Promise<false | ProtoRoute[]>;
 export {};
